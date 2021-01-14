8197dc320bf3fbda342548579d1406e0d2f715bc
Kush Gandhi <kush25698@gmail.com>
2021-01-13 22:37:06 -0800

    Handling User authentications upload

---
+++ src/main/java/com/hcl/model/User.java
@@ -0,0 +1,18 @@
+package com.hcl.model;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@Builder
+
+public class User {
+	
+	
+	private String userName;
+	
+	private String password;
+	
+
+}
